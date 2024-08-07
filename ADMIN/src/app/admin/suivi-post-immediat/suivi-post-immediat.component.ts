1b2f973e8766e3df8baa3612b79ebe91ddf920ac
Mouhamed-bengayed <76977734+Mouhamed-bengayed@users.noreply.github.com>
2024-08-07 15:23:45 +0200

    07/8/

--- ADMIN/src/app/admin/suivi-post-immediat/suivi-post-immediat.component.ts
+++ ADMIN/src/app/admin/suivi-post-immediat/suivi-post-immediat.component.ts
@@ -19,7 +19,8 @@ export class SuiviPostImmediatComponent implements OnInit {
   checkboxControl6 = new FormControl(false);
   checkboxControl7 = new FormControl(false);
   checkboxControl8 = new FormControl(false);
-  checkboxControl15 = new FormControl(false);
+  checkboxControl9 = new FormControl(false);
+
   @ViewChild('picker1') picker1!: MatDatepicker<any>;
   picker2!: MatDatepicker<any>;
   picker4!: MatDatepicker<any>;
@@ -33,11 +34,10 @@ export class SuiviPostImmediatComponent implements OnInit {
 
   firstFormGroup = new FormGroup({
      date_chirurgie: new FormControl(''),
-     date_suiui: new FormControl(''),
-
+     date_suivi: new FormControl(''),
     dissectomie_etage: new FormControl(''),
       liberation_post_etage: new FormControl(''),
-      arthrodese_etage: new FormControl(''),
+      arthrodese_etage: this.fb.array([]),
       complications_per_operatoire: new FormControl(''),
       complications_per_operatoire_auter: new FormControl(''),
     
@@ -45,45 +45,32 @@ export class SuiviPostImmediatComponent implements OnInit {
   
  
   symptomatologieFormGroup = new FormGroup({
-    dissectomi: new FormControl(''),
-    liberation_post: new FormControl(''),
-    arthrodese: new FormControl(''),
-    trouble_vesico_sphincteriens: new FormControl(''),
+    lombalgie: new FormControl(''),
+    sciatique: new FormControl(''),
+    cruralgie: new FormControl(''),
+    trouble_vesico_sphincteriens: this.fb.array([]),
     trouble_vesico_sphincteriens_Incontinence_urinaire: new FormControl(''),
 
 
-
-   
-   
-   
   });
 
 thridFormGroup = new FormGroup({
     
    
-    testing_musculaire_L2: new FormControl(''),
-    testing_musculaire_L3: new FormControl(''),
-    testing_musculaire_L4: new FormControl(''),
-    testing_musculaire_L5: new FormControl(''),
-    testing_musculaire_S1: new FormControl(''),
-    sensibilte_L2: new FormControl(''),
-    sensibilte_L3: new FormControl(''),
-    sensibilte_L4: new FormControl(''),
-    sensibilte_L5: new FormControl(''),
-    sensibilte_S1: new FormControl(''),
+    testing_musculaire_l2: new FormControl(''),
+    testing_musculaire_l3: new FormControl(''),
+    testing_musculaire_l4: new FormControl(''),
+    testing_musculaire_l5: new FormControl(''),
+    testing_musculaire_s1: new FormControl(''),
+    sensibilte_musculaire_l2: new FormControl(''),
+    sensibilte_musculaire_l3: new FormControl(''),
+    sensibilte_musculaire_l4: new FormControl(''),
+    sensibilte_musculaire_l5: new FormControl(''),
+    sensibilte_musculaire_s1: new FormControl(''),
     examen_perinee_sensibilite: new FormControl(''),
     examen_perinee_Tonus_anal: new FormControl(''),
     examen_perinee_Reflexe_anal : new FormControl(''),
-    intensite_douleur: new FormControl(''),
-    soins_personnels: new FormControl(''),
-    levee: new FormControl(''),
-    marche: new FormControl(''),
-    assis: new FormControl(''),
-    debout: new FormControl(''),
-    sommeil: new FormControl(''),
-    vie_sexuelle: new FormControl(''),
-    vie_sociale: new FormControl(''),
-    voyage: new FormControl(''),
+   
    
 
   });
@@ -94,8 +81,8 @@ thridFormGroup = new FormGroup({
       localStorage.setItem('symptomatologieFormGroupData', JSON.stringify(symptomatologieFormGroupData));
   }
   savethridForm() {
-    const thridFormGroupData1 = this.thridFormGroup.value;
-      localStorage.setItem('thridFormGroupData', JSON.stringify(thridFormGroupData1));
+    const thridFormGroupData = this.thridFormGroup.value;
+      localStorage.setItem('thridFormGroupData', JSON.stringify(thridFormGroupData));
    }
   savesFirstFormGroup(){
     const firstFormGroupData = this.firstFormGroup.value;
@@ -103,25 +90,27 @@ thridFormGroup = new FormGroup({
   }
 
   addsuiviImed() {
+    this.savethridForm();
        // Récupérer les données des formulaires depuis le localStorage
- const FirstFormData = JSON.parse(localStorage.getItem('FirstFormData') || '{}');
- const secondFormGroupData = JSON.parse(localStorage.getItem('secondFormGroupData') || '{}');
+ const FirstFormData = JSON.parse(localStorage.getItem('firstFormGroup') || '{}');
  const symptomatologieFormGroupData = JSON.parse(localStorage.getItem('symptomatologieFormGroupData') || '{}');
+ const thridFormGroupData = JSON.parse(localStorage.getItem('thridFormGroupData') || '{}');
 
  
    // Fusionner les données des formulaires avec les données du patient
 
    const patientData = {
     ...FirstFormData,
-    ...secondFormGroupData,
     ...symptomatologieFormGroupData,
+    ...thridFormGroupData,
+   
 
   };
-  
+
       this.patientService.createsuiviImedia(patientData).subscribe(
         (response) => {
           console.log("Patient enregistré avec succès : ", response);
-    localStorage.removeItem('FirstFormData');
+    localStorage.removeItem('firstFormGroup');
     localStorage.removeItem('symptomatologieFormGroupData');
     localStorage.removeItem('thridFormGroupData');
         },
